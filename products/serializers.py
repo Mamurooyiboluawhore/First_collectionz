78f6d2302b70ced863a3dfcad2ebd58583e15a34
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-01-22 15:15:52 +0100

    .\products\

---
+++ products/serializers.py
@@ -0,0 +1,8 @@
+# serializers.py
+from rest_framework import serializers
+from accounts.models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
---
+++ products/views.py
@@ -0,0 +1,57 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.models import Product
+from .serializers import ProductSerializer
+
+class ProductListCreateAPIView(APIView):
+    def get(self, request, format=None):
+        try:
+            products = Product.objects.all()
+            serializer = ProductSerializer(products, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def post(self, request, format=None):
+        try:
+            serializer = ProductSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class ProductDetailAPIViews(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def delete(self, request, pk):
+        try:
+            product = get_object_or_404(Product, pk=pk)
+            product.delete()
+            return JsonResponse({'message': 'Product deleted successfully.'})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
