0972a9f7f284857027efa0bd75f1f1ecd928c3bc
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-01-30 15:12:21 +0100

    fixed bugs in settings.py

--- core/settings.py
+++ core/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'core.urls',
+ROOT_URLCONF = 'core.urls'
             
 
 
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DBs_PASSWORD'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': int(os.getenv('DB_PORT', 5432)),
     }
