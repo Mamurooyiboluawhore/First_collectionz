2cac2014535deaf2702930903311ff5920794dda
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-03-02 00:14:09 +0100

    URL Configuration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, status, viewsets
 from rest_framework.response import Response 
 from rest_framework.views import APIView
+from rest_framework import status
 from django.contrib.auth import get_user_model
+from .utils import generate_otp, send_otp_email
 from .serializers import (
 	RegisterUserSerializer,
 	UserSerializer,
@@ -11,12 +13,54 @@ from .serializers import (
 from .permissions import IsAccountOwner
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import authenticate
 
 User = get_user_model()
 
 
 
+class LoginWithOTP(APIView):
+	def post(self, request):
+		email = request.data.get('email', '')
+		try:
+			user = User.objects.get(email=email)
+		except User.DoesNotExist:
+			return Response({'error': 'User with this email does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+		
+		otp = generate_otp()
+		user.otp = otp
+		user.save()
+
+		send_otp_email(email, otp)
+
+		return Response({'message': 'OTP has been sent to your email.'}, status=status.HTTP_200_OK)
+	
+
+
+class ValidateOTP(APIView):
+    def post(self, request):
+        email = request.data.get('email', '')
+        otp = request.data.get('otp', '')
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'error': 'User with this email does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+
+        if user.otp == otp:
+            user.otp = None  # Reset the OTP field after successful validation
+            user.save()
+
+            # Authenticate the user and create or get an authentication token
+            token, _ = Token.objects.get_or_create(user=user)
+
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid OTP.'}, status=status.HTTP_400_BAD_REQUEST)
+		
+		
 class UserCreateAPIView(generics.CreateAPIView):
 	queryset = User.objects.all()
 	serializer_class = RegisterUserSerializer
