a60880ee70a2aee74b579784d042a993ded3bc2b
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-04-05 23:03:40 +0100

    updated account

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,11 @@ from .utils import generate_otp, send_otp_email
 from .serializers import (
 	RegisterUserSerializer,
 	UserSerializer,
-	ChangePasswordSerializer
+	ChangePasswordSerializer,
+	ValidateResetPasswordSerializer,
+	ConfirmPasswordResetSerializer,
+	ResetPasswordEmailSerializer
+	
 )
 
 from .permissions import IsAccountOwner
@@ -177,3 +181,88 @@ class ChangePasswordAPIView(generics.GenericAPIView):
 			)
 
 change_password = ChangePasswordAPIView.as_view()
+
+
+class PasswordResetAPIView(generics.GenericAPIView):
+	serializer_class = ResetPasswordEmailSerializer
+
+	def post(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		email = serializer.validated_data.get('email', '')
+		try:
+			user = User.objects.get(email=email)
+		except:
+			return Response({'error': 'User with this email does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+		otp = generate_otp()
+		user.otp = otp
+		user.save()
+
+		send_otp_email(email, otp)
+
+		return Response({
+			'message': 'OTP has been sent to your email',
+			'status': status.HTTP_200_OK
+		}, status=status.HTTP_200_OK)
+
+password_reset = PasswordResetAPIView.as_view()
+
+class ValidatePasswordResetOTPAPIView(generics.GenericAPIView):
+	serializer_class = ValidateResetPasswordSerializer
+
+	def post(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		email = serializer.validated_data.get('email', '')
+		otp = serializer.validated_data.get('otp', '')
+		try:
+			user = User.objects.get(email=email)
+		except:
+			return Response({'error': 'User with this email does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+		if user.otp == otp:
+			return Response({
+				'message': 'OTP is valid',
+				'status': status.HTTP_200_OK
+			}, status=status.HTTP_200_OK)
+		return Response({
+			'error': 'Invalid OTP',
+			'status':status.HTTP_400_BAD_REQUEST
+		}, status=status.HTTP_400_BAD_REQUEST)
+
+validate_password_reset_otp = ValidatePasswordResetOTPAPIView.as_view()
+
+class ConfirmPasswordResetAPIView(generics.GenericAPIView):
+	serializer_class = ConfirmPasswordResetSerializer
+
+	def post(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		email = serializer.validated_data.get('email', '')
+		otp = serializer.validated_data.get('otp', '')
+		password_1 = serializer.validated_data.get('password_1', '')
+		password_2 = serializer.validated_data.get('password_2', '')
+		try:
+			user = User.objects.get(email=email)
+		except:
+			return Response({'error': 'User with this email does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+		if user.otp == otp:
+			if password_1 == password_2:
+				user.otp = None
+				user.set_password(serializer.validated_data.get('password_1'))
+				user.save()
+				update_session_auth_hash(request, user)
+				return Response(
+					{'message': 'Password reset succesful'},
+					status=status.HTTP_200_OK
+				)
+			return Response({
+				'error': 'password_1 not same as password_2',
+				'status':status.HTTP_400_BAD_REQUEST
+			}, status=status.HTTP_400_BAD_REQUEST)
+		return Response({
+			'error': 'Invalid OTP',
+			'status':status.HTTP_400_BAD_REQUEST
+		}, status=status.HTTP_400_BAD_REQUEST)
+
+confirm_password_reset = ConfirmPasswordResetAPIView.as_view()
+
