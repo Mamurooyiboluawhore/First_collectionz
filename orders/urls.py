c4b198081a02fcd0e2c0a00476b86c7d5a292777
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-04-13 22:08:01 +0100

    updated urls.py in orders

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,9 @@ from django.urls import include, path
 from .views import OrderListCreateAPIView
 
 urlpatterns = [
-    path('', OrderListCreateAPIView.as_view(), name='get-order'),
+    path('', OrderListCreateAPIView.as_view(), name='order-list-create'),
+    path('<uuid:pk>/', OrderListCreateAPIView.as_view(), name='get-order'),
+    path('<uuid:pk>/update/', OrderListCreateAPIView.as_view(), name='update-order'),
+    path('<uui:pk>/delete/', OrderListCreateAPIView.as_view(), name='delete-order'),
+    
 ]
