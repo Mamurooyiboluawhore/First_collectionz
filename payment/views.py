b204ac33b36073ac6f60b328ad64c3b044b8bdbb
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-04-18 15:01:30 +0100

    implementing payment

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,15 @@
 from rave_python import Rave, RaveExceptions, Misc
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from accounts.models import Order, User
+import requests
+from .serializers import OrderCreateSerializer, OrderItemsSerializer, OrderSerializer
+from rest_framework.permissions import IsAuthenticated
+import uuid
+
+
 import os
 
 rave = Rave(os.getenv("Public_key"))
@@ -47,3 +58,79 @@ except RaveExceptions.TransactionValidationError as e:
 except RaveExceptions.TransactionVerificationError as e:
     print(e.err["errMsg"])
     print(e.err["txRef"])
+
+
+def initiate_payment(amount, email, order_id):
+    url = "https://api.flutterwave.com/v3/payments"
+    headers = {
+        "Authorization": f"Bearer {os.getenv('Secret_key')}"    
+    }
+    
+    data = {
+        "tx_ref": str(uuid.uuid4()),
+        "amount": str(amount), 
+        "currency": "USD",
+        "redirect_url": "http:/127.0.0.1:8000/api/orders/confirm_payment/?o_id=" + order_id,
+        "meta": {
+            "consumer_id": 23,
+            "consumer_mac": "92a3-912ba-1192a"
+        },
+        "customer": {
+            "email": email,
+            "phonenumber": "080****4528",
+            "name": "Yemi Desola"
+        },
+        "customizations": {
+            "title": "Pied Piper Payments",
+            "logo": "http://www.piedpiper.com/app/themes/joystick-v27/images/logo.png"
+        }
+    }
+    
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response_data = response.json()
+        return Response(response_data)
+    
+    except requests.exceptions.RequestException as err:
+        print("the payment didn't go through")
+        return Response({"error": str(err)}, status=500)
+    
+
+@csrf_exempt
+@api_view(['POST'])
+def confirm_payment(request, o_id):
+    """
+    Confirm a payment made to an order using Flutterwave Transaction Reference (tx_ref).
+    The tx_ref is sent in the redirect URL when the user makes a successful payment on Flutterwave.
+    This view returns the status of that transaction and updates the Order model with the appropriate status if it was successful or failed.
+    This view returns a JSON object with information about the transaction if it exists and has not been confirmed yet.
+    This view returns the transaction details if it exists and has not been confirmed yet.
+    If the transaction does not exist or has already been confirmed, it will return a message saying so.
+    :param request: HTTP Request from client
+    :type request: HttpRequest
+    :param o_id: Order ID of the order being paid for
+    :type o_id: int
+    :return: JSON object containing transaction data if it exists and hasn't been confirmed
+    Message stating that the transaction doesn't exist or has been confirmed
+    :rtype: JsonResponse
+    """
+    url = f"https://api.flutterwave.com/v3/transactions/{o_id}"
+    headers = {"Authorization":"Bearer FLWSECK_TEST-d6fadbebeccddc9cd2bfbd9eeaeff3fd"} #
+    params = {'flw_test': True}
+    try:
+        resp = requests.get(url, headers=headers, params=params)
+        if resp.status_code == 200:
+            data = resp.json()
+            if 'data' in data and len(data['data']) > 0:
+                trans = data['data'][0]
+                if trans['meta']['verification']['status'] != 'successful':
+                    return Response({"message": "Transaction Not Successfully Verified"}, status=400)
+                elif 'message' in data:
+                    return Response(data)
+                else:
+                    return Response(trans)
+            
+    except Exception as e:
+        return Response({"Error":str(e)})
+            
