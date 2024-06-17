f152d3706623f72aeca0075f26a7ba80ecb0017f
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-06-17 15:18:28 +0100

    removed the default permission in settings.py

--- core/settings.py
+++ core/settings.py
@@ -174,9 +174,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
 }
 
 SIMPLE_JWT = {
