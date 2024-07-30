820c89af5abfe3b4bb68f72fc51874d1d4c367f6
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-07-30 14:33:37 +0100

    updated category urls

--- category/urls.py
+++ category/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', CategoryCreateAPIView.as_view(), name='category-list-create'),
     path('<uuid:pk>/', CategoryDetailAPIViews.as_view(), name='category-detail'),
     path('<uuid:pk>/details/', CategoryDetailAPIViews.as_view(), name='category-details'),
-    path('<int:category_pk>/category/', ProductByCategory.as_view(), name='product-by-category'),
+    path('<int:category_pk>/', ProductByCategory.as_view(), name='product-by-category'),
     path('<uuid:pk>/delete/', CategoryDetailAPIViews.as_view(), name='category-delete'),
 ]
